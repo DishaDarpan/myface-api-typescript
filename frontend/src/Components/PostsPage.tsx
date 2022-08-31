6d1afe74957762201500755721b91f57b449f807
Jack Hughes <Jack.Hughes@softwire.com>
2022-08-31 15:58:20 +0100

    postpage works

--- frontend/src/Components/PostsPage.tsx
+++ frontend/src/Components/PostsPage.tsx
@@ -1,20 +1,51 @@
 import React ,{useState, useEffect} from 'react';
-import {PostModel} from 'C:/Training1/MYFACE(REACT)/myface-api-typescript/src/models/api/postModel';
+
+interface PostUserModel {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+}
+
+interface PostModel {
+    id: number;
+    message: string;
+    imageUrl: string;
+    createdAt: string;
+    postedBy: PostUserModel;
+    likedBy: PostUserModel[];
+    dislikedBy: PostUserModel[];
+}
+
+interface PostModelReturn {
+    results: PostModel[],
+    next: string,
+    previous: string,
+    total: number,
+}
 
 function PostsPage(){
 
-    const [postList, setPostList] = useState<PostModel>()
+    const [data, setData] = useState<PostModelReturn>();
 
-    useEffect(() => {
-        fetch("http://localhost:3001/posts").then(response => response.json()).then(data => setPostList(data));
-    }, [])
+    useEffect(
+        () => {
+            fetch("http://localhost:3001/posts")
+                .then(response => response.json())
+                .then(data => setData(data));
+        }, []
+    );
 
-    if(!postList){
-        return <div>No Data Found!</div>
-    }
-    
-    return (
-        <h1>Posts Page</h1>
-    )
+    return (<>
+        {data?.results.map((post) => {
+            return (
+                <div key={post.id}>
+                    <h3>{post.postedBy.name}</h3>
+                    <img src={post.imageUrl}/>
+                    <h3>{post.message}</h3> 
+                </div>)
+        })
+            
+        }</>)
 }
 export default PostsPage;
